f5b785b6d28651165761d1e3a0f69b712f08147f
paulina lydwine d <paulinalyd@eduroam-int-dhcp-97-82-254.ucl.ac.uk>
2023-10-25 13:37:33 +0100

    feat: init starter code

---
+++ database.php
@@ -0,0 +1,9 @@
+function connectToDatabase($host, $username, $password, $database) {
+    $conn = mysqli_connect($host, $username, $password, $database);
+
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
+
+    return $conn;
+}
