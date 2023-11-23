a77b3cee2ee73d1239df70c3dd2ac82ad6fda0f9
paulinalydav <paulina.davita.23@ucl.ac.uk>
2023-11-23 19:10:49 +0000

    fix: move session helper

--- helper/session_check.php
+++
@@ -1,9 +0,0 @@
-
-<?php
-  session_start();
-
-  if (!isset($_SESSION) || $_SESSION == null) {
-    echo('<div class="text-center">You\'re not logged in. Please re-login if this was a mistake</div>');
-    header('refresh:3;url=browse.php');
-}
-?>
