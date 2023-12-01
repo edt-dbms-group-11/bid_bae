5fbf81932d92ee4f3abc4760e1533a4249e79d68
gazalguptaa <60865547+gazalguptaa@users.noreply.github.com>
2023-12-01 19:16:59 +0000

    Fix additional files

--- database.php
+++
@@ -1,7 +0,0 @@
-<?php
-  $connection = mysqli_connect("localhost", "root", "", "auction_system");
-
-  if (mysqli_connect_errno())
-    echo 'Failed to connect to the MySQL server: '. mysqli_connect_error();
-
-?>
