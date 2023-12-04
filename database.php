cc799f3ed9eb98030467b307468fdb4471078455
Anshuman Agarwal <dev.anshuman73@gmail.com>
2023-12-04 01:47:12 +0000

    Update database.php

--- database.php
+++ database.php
@@ -1,6 +1,9 @@
 <?php
-  $connection = mysqli_connect("localhost", "root", "root", "auction_db_test");
-
+  $serverhost = getenv('DB_HOST');
+  $username = getenv('DB_USER');
+  $password = getenv('DB_PASS');
+  $dbname = getenv('DB_NAME');
+  $connection = mysqli_connect($serverhost, $username, $password, $dbname);
   if (mysqli_connect_errno())
     echo 'Failed to connect to the MySQL server: '. mysqli_connect_error();
 
